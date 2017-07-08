3cc78acda01ab9260f876a03788151e57acd0af8
Yann Druffin <yann.druffin@laposte.net>
2017-07-08 13:27:22 -0700

    Added exception

--- TemperatureLibrary/Converter/TemperatureConverter.cs
+++ TemperatureLibrary/Converter/TemperatureConverter.cs
@@ -18,15 +18,32 @@ namespace TemperatureLibrary.Converter
 
         public ITemperature Convert(ITemperature temperature, Unit unit)
         {
+            if(converterFactory == null)
+            {
+                throw new Exception("There is no converters defined.");
+            }
+
             if(temperature.Unit ==  unit)
             {
                 return temperature;
             }
 
-            var tempInKelvin = converterFactory.GetConverter(temperature.Unit).ToKelvin(temperature);
+            var fromConverter = GetConverter(temperature.Unit);
+            var tempInKelvin = fromConverter.ToKelvin(temperature);
+            var toConverter = GetConverter(unit);
 
+            return toConverter.FromKelvin(tempInKelvin);
+        }
+
+        private IUnitConverter GetConverter(Unit unit)
+        {   
             var converter = converterFactory.GetConverter(unit);
-            return converter.FromKelvin(tempInKelvin);
+            if(converter == null)
+            {
+                throw new Exception(string.Format("No Converter were found for this unit: {0}, please contact the dev, or create a new converter for the unit",unit));
+            }
+            return converter;
+
         }
     }
 }
