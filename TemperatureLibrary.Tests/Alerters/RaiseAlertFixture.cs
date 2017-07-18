cfe6d465c534a0a69ca3547645363a28fc108f5e
yann Druffin <yann.druffin@gmail.com>
2017-07-18 16:13:32 -0700

    Added RaiseAlert

---
+++ TemperatureLibrary.Tests/Alerters/RaiseAlertFixture.cs
@@ -0,0 +1,140 @@
+﻿using TemperatureLibrary.Alerters;
+using Xunit;
+
+namespace TemperatureLibrary.Tests.Alerters
+{
+    public class RaiseAlertFixture
+    {
+        private readonly RaiseAlert alert;
+        private const string alertName = "RaiseAlert";
+
+        public RaiseAlertFixture()
+        {
+            alert = new RaiseAlert(alertName, 10.0m, 0.5m);
+        }
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            Assert.Equal(alertName, alert.Name);
+            Assert.Equal(10.0M, alert.ThresholdTemperature);
+            Assert.Equal(0.5M, alert.MinimumReleventFluctuation);
+            Assert.Equal(false, alert.IsAlertOn);
+        }
+
+        [Fact]
+        public void TemperatureRaisedToThresholdValueShouldReturnTrue()
+        {
+            var result = alert.IsConditionReached(10.0M, 0.5M);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TemperatureDropsToThresholdValueShouldReturnFalse()
+        {
+            var result = alert.IsConditionReached(10.0M, -0.5M);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TemperaturemDidNotReachThresholdValueShouldReturnFalse()
+        {
+            var result = alert.IsConditionReached(11.0M, 0.5M);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TemperatureReachedThresholdValueMutlipleTimesWithInsignificantDropFluctuationShouldReturnOneAlert()
+        {
+            var result = alert.IsConditionReached(10.0M, 0.5M);
+            Assert.True(result);
+
+            result = alert.IsConditionReached(9.8M, -0.2M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(10.0M, 0.2M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(9.5M, -0.5M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(10.0M, 0.5M);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TemperatureReachedThresholdValueMutlipleTimesWithInsignificantRaiseFluctuationShouldReturnOneAlert()
+        {
+            var result = alert.IsConditionReached(10.0M, 0.5M);
+            Assert.True(result);
+
+            result = alert.IsConditionReached(10.2M, 0.2M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(10.0M, -0.2M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(10.5M, 0.5M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(10.0M, -0.5M);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TemperatureReachesThresholdValueMutlipleTimesWithSignificantDropFluctuationShouldReturnMultipleAlert()
+        {
+            var result = alert.IsConditionReached(10.0M, 0.5M);
+            Assert.True(result);
+
+            result = alert.IsConditionReached(9.0M, -1M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(10.0M, 1M);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TemperatureReachesThresholdValueMutlipleTimesWithSignificantRaiseFluctuationShouldReturnOneAlert()
+        {
+            var result = alert.IsConditionReached(10.0M, 0.5M);
+            Assert.True(result);
+
+            result = alert.IsConditionReached(11.0M, 1M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(10.0M, -1M);
+            Assert.False(result);
+        }
+
+        /// <summary>
+        /// In this test I assume that if the temperature reaches the threshold value and continues to rise and then the temperature drops
+        /// directly below the threshold value and returns to the threshold value it should return true only if the temperature 
+        /// exceded the minimum relevent fluctuation. 
+        /// </summary>
+        [Fact]
+        public void TemperatureReachesThresholdValueAndContinuesToRiseAndDropDirectlyUnderThresholdValue()
+        {
+            var result = alert.IsConditionReached(10.0M, 0.5M);
+            Assert.True(result);
+
+            result = alert.IsConditionReached(100.0M, 90.0M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(9.8M, -90.2M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(10M, 0.2M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(100.0M, 90.0M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(9M, -90.0M);
+            Assert.False(result);
+
+            result = alert.IsConditionReached(10M, 1.0M);
+            Assert.True(result);
+        }
+    }
+}
---
+++ TemperatureLibrary/Alerters/RaiseAlert.cs
@@ -0,0 +1,46 @@
+﻿
+namespace TemperatureLibrary.Alerters
+{
+    /// <summary>
+    /// Represents an alert issued only when the threshold is reached when the temperature is rising.
+    /// </summary>
+    public class RaiseAlert : AlertBase, IAlerter
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="alertName">The name of the alert to identifiy it.</param>
+        /// <param name="thresholdTemperature"> The threshold value of the alert.</param>
+        /// <param name="minimumReleventFluctuation">The minimum fluctuation considered relevent for alert.</param>
+        public RaiseAlert(string alertName, decimal thresholdTemperature, decimal minimumReleventFluctuation)
+            : base(alertName, thresholdTemperature, minimumReleventFluctuation) { }
+
+        /// <summary>
+        /// Check if the condition is reached to issue the alert.
+        /// </summary>
+        /// <param name="temperature">The temperature to check.</param>
+        /// <param name="fluctuation">The last fluctuation of the thermometer.</param>
+        /// <returns>A boolean if the condition is reached to spread the alert.</returns>
+        public bool IsConditionReached(decimal temperature, decimal fluctuation)
+        {
+
+            if (temperature != ThresholdTemperature)
+            {
+                if (!IsAlertOn)
+                    return false;
+                if (ThresholdTemperature - MinimumReleventFluctuation > temperature)
+                    IsAlertOn = false;
+                return false;
+            }
+
+            if (IsAlertOn)
+                return false;
+
+            if (fluctuation <= 0)
+                return false;
+
+            IsAlertOn = true;
+            return true;
+        }
+    }
+}
