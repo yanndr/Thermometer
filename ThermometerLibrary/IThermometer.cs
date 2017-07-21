49c72da097f55a59f8542e3d6d824001bcf8e155
yann Druffin <yann.druffin@gmail.com>
2017-07-21 10:52:09 -0700

    Fix Ithermometer,

--- ThermometerLibrary/IThermometer.cs
+++ ThermometerLibrary/IThermometer.cs
@@ -4,6 +4,13 @@ using ThermometerLibrary.Converter;
 
 namespace ThermometerLibrary
 {
+    public interface IThermometer
+    {
+        Unit ThermometerUnit { get; set; }
+        ITemperature Temperature { get; }
+        void UpdateTemperature(ITemperature temperature);
+    }
+
     public interface IMultiUnitThermometer
     {
         ITemperatureConverter Converter {get;}
